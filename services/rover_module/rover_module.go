aa20bfbabbe355f76a7b6574617435b5ec689f1e
framcyber <fram.cyber@gmail.com>
2022-05-15 00:51:25 +0700

    Changed the idea of the rover system to operate with degree and comparison to direction

--- services/rover_module/rover_module.go
+++ services/rover_module/rover_module.go
@@ -11,11 +11,32 @@ type RoverService interface {
 }
 
 func InitialDP() entities.CurrentDP {
-	return entities.CurrentDP{}
+	initDP := entities.CurrentDP{
+		Degree:     90,
+		Position_X: 0,
+		Position_Y: 0,
+	}
+	return initDP
 }
 
 func Rotate(c entities.CurrentDP, d string) entities.CurrentDP {
-	return entities.CurrentDP{}
+
+	// Set to revert the circle
+	if d == "R" && c.Degree == 0 {
+		c.Degree = 360
+	}
+
+	// Calculate the next degree follow by the rotate instruction rules
+	nextDegree := c.Degree + entities.Instruction.RotateInstruction[d]
+
+	// Reset degree if completed the circle
+	if d == "L" && nextDegree == 360 {
+		nextDegree = 0
+	}
+
+	c.Degree = nextDegree
+
+	return c
 }
 
 func Move(c entities.CurrentDP, m string, u int) entities.CurrentDP {
